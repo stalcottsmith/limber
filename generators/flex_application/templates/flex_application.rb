a9e99bc305222c6c296ab015bbc11135f0f4e61e
Steven Talcott Smith <steve@talcottsystems.com>
2008-05-30 18:52:50 -0400

    improved example app; added comments

--- generators/flex_application/templates/flex_application.rb
+++ generators/flex_application/templates/flex_application.rb
@@ -1,18 +1,19 @@
 #
 # This filed defines the top level of your application.
 #
-# Components of this application are defined in the app/flex/<%= file_name %> directory.
-#
 
 class <%= file_name.camelcase %> < Limber::Components::Application
   
   has_attributes :height => "100%", :width => "100%"
   
-  has_components :progress_popup
+  # tells Limber to look for these components in app/flex/<%= file_name %>
+  has_components :progress_popup #, :component2, :component3
   
-  # If this method is defined in your component, the ActionScript is 
-  # included at the top of the .mxml file.  The debug() function is
-  # necessary only in the top-level Application component.
+  # If a Limber component implements a custom_action_script method,
+  # the string returned is added to the top of the MXML file in the script
+  # definition area.  Ie. Between <script></script> tags along
+  # with any automatically generated ActionScript
+  # ...
   def custom_action_script
     return <<-END_AS
       import mx.controls.Alert;
@@ -22,8 +23,23 @@ class <%= file_name.camelcase %> < Limber::Components::Application
     END_AS
   end
   
+  # The :to_mxml method is where the magic happens.
+  #  - Limber invokes this method to generate the MXML.
+  #  - Ruby’s block structure is used to indicate XML containment hierarchy.
+  #  - Unrecognized methods are assumed to correspond to ActionScript classes.
+  #  - Hash parameters turn into XML attributes.
+  #  - Many shortcuts have been implemented to make attributes more readable
+  #    and to reduce code size.  Documentation forthcoming...
+
   def to_mxml
-    label :text => "Hello World!"
+    # non-UI components must go at the top
+    fade :id => 'fadeIn', :duration => 100
+    style :source => 'com/<%= file_name %>/css/<%= file_name %>.css'
+    
+    # UI Components
+    h_box(:height => "100%", :vertical_align => "center") {
+      label :text => "Hello World!", :font_size => 42
+    }
   end
   
 end
